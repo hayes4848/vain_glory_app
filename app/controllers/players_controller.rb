7486dabf7edbedd10ec3130c15b62020856b5618
Cody Stringham <cs.negativ@gmail.com>
2017-03-18 11:13:04 -0600

    installs react-rails

---
+++ app/controllers/players_controller.rb
@@ -0,0 +1,15 @@
+class PlayersController < ApplicationController
+  def index
+    # https://api.dc01.gamelockerapp.com/shards/na/players
+    conn = Faraday.new(url: 'https://api.dc01.gamelockerapp.com', headers: {
+      "Authorization" => "Bearer #{ENV['API_KEY']}",
+      "X-TITLE-ID" => 'semc-vainglory',
+      "Accept" => 'application/vnd.api+json'} )
+    response = conn.get('/shards/na/players').body
+    json = JSON.parse(response)
+    @results = Hashie::Mash.new(json)
+  end
+
+  def show
+  end
+end
