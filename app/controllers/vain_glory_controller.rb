c3b8b379382fbc99335c12b1cfca548a2c650942
Cody Stringham <cs.negativ@gmail.com>
2017-03-18 10:42:54 -0600

    spaghetti code

--- app/controllers/vain_glory_controller.rb
+++ app/controllers/vain_glory_controller.rb
@@ -1,10 +1,11 @@
-class VainGloryController < ApplicationController
+ class VainGloryController < ApplicationController
 
   def basic_vain_response
     # https://api.dc01.gamelockerapp.com/shards/na/players
-    conn = Faraday.new(url: 'https://api.dc01.gamelockerapp.com', headers: {"Authorization" => "Bearer #{ENV['API_KEY']}", "X-TITLE-ID" => 'semc-vainglory', "Accept" => 'application/vnd.api+json'} ) 
-    response = conn.get '/shards/na/players/6abb30de-7cb8-11e4-8bd3-06eb725f8a76' 
-    raise 'it'
+    conn = Faraday.new(url: 'https://api.dc01.gamelockerapp.com', headers: {"Authorization" => "Bearer #{ENV['API_KEY']}", "X-TITLE-ID" => 'semc-vainglory', "Accept" => 'application/vnd.api+json'} )
+    response = conn.get('/shards/na/players').body
+    json = JSON.parse(response)
+    @results = Hashie::Mash.new(json)
   end
 
 end
